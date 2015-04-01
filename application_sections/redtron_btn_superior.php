624a8926549f141c14e8657bdd3ea759ac6a3c7b
programando <jhonjamesmg@hotmail.com>
2015-04-01 15:16:59 -0500

    Jhon James

--- application_sections/redtron_btn_superior.php
+++ application_sections/redtron_btn_superior.php
@@ -3,7 +3,7 @@
     <div class="col-lg-12  col-md-12  col-sm-12">
      <div class="tabs">
       <a class="tab-red"    href="<?= BASE_URL ;?>redtron/red_de_amigos_tron"><div class="campos-pagina-red">RED DE AMIGOS TRON</div></a>
-      <a class="tab-tienda" href="<?= BASE_URL ;?>redtron/Planes_de_Registro"><div class="campos-pagina-tienda">  TIENDA TRON  </div></a>
+      <a class="tab-tienda" href="<?= BASE_URL ;?>redtron/planes_de_registro"><div class="campos-pagina-tienda">  TIENDA TRON  </div></a>
     </div>
     </div>
 <!--Campos De La Pagina RED DE AMIGOS TRON-->
