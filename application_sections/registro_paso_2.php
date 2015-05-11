129fef776132d399fcdede8fe0a20c577230954c
erickdav <erickdavidmg@gmail.com>
2015-05-11 17:25:27 -0500

    Erick David Montaño

--- application_sections/registro_paso_2.php
+++ application_sections/registro_paso_2.php
@@ -3,7 +3,6 @@
     Js          => Carpeta Js  => tron_terceros_registro.js
 -->
 
-
 <div  class="col-lg-12"><!--Contenedor de la pagina -->
    <div class="cont-tabla-planes"><!--Fila -->
 
@@ -13,15 +12,46 @@
              <thead><!--Emcabezado -->
                  <tr>
                      <th>BENEFICIOS</th>
-                     <th class="text-center"><a href="#">PLAN COMPRADOR OCASIONAL</a></th>
-                     <th class="text-center"><a href="#">PLAN CLIENTE TRON</a></th>
-                     <th class="text-center"><a href="#">PLAN EMPRESARIO TRON</a></th>
+                     <th class="text-center" >
+                        <a class="nuvv"
+                           title="
+                                <img src='<?= BASE_IMG_EMPRESA ;?>img_tooltips1.png' style='float:left; margin:0 20px 10px 0' /> <br />
+                                Para aquellas personas que aún no han descubierto los beneficios de consumir los productos TRON.
+                                <br style='clear:both;' /> ">
+                                <span class="aS colurAd">
+                                    PLAN COMPRADOR<br />OCASIONAL
+                               </span>
+                        </a>
+                    </th>
+
+                     <th class="text-center">
+                         <a class="nuvv" title="
+                            <img src='<?= BASE_IMG_EMPRESA ;?>img_tooltips2.png' style='float:left; margin:0 20px 10px 0' />
+                                Para aquellas personas que se quieren beneficiar de lo que brinda TRON pero aún no han comprendido la facilidad con que podrían recibir ingresos adicionales. ">
+                             <span class="aS colurAd">
+                              PLAN CLIENTE <br />TRON
+                             </span>
+                        </a>
+                    </th>
+                     <th class="text-center">
+                        <a class="nuvv" title="
+                            <img src='<?= BASE_IMG_EMPRESA ;?>img_tooltips3.png' style='float:left; margin:0 20px 10px 0' />
+                            Para aquellas personas que quieren beneficiarse de TODO lo que brinda TRON y desea compartirlo con sus familiares y amigos.
+                            <br style='clear:both;' /> ">
+                            <span class="aS colurAd">
+                                 PLAN EMPRESARIO <br /> TRON
+                            </span>
+                        </a></th>
                  </tr>
              </thead><!--Emcabezado -->
 
              <tbody><!--Cuerpo de la tabla -->
                 <tr>
-                   <td> <strong><a href="#">PRECIOS ESPECIALES PARA AMIGOS TRON</a></strong> </td>
+                   <td> <strong>
+                          <a class="nuvv" title="
+                              Beneficio que adquieren aquellas personas que consumen mensualmente los productos TRON de aseo y desinfección para el hogar o los productos de la línea industrial fabricados por Tecnologías Aplicadas S.A. ">
+                              <span class="aS colurAd">PRECIOS ESPECIALES PARA AMIGOS TRON</span>
+                          </a></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
@@ -34,7 +64,11 @@
                 	<td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
                 </tr>
                 <tr>
-                   <td> <strong><a href="#">PASES DE CORTESIA Y PASES PREMIUM</a></strong> </td>
+                   <td> <strong>
+                        <a class="nuvv aS colurAd" title="
+                              Para que le otorgue Precios Especiales en toda la tienda a tus amigos potenciales a unirse a la red TRON. ">
+                              PASES DE CORTESÍA Y PASES PREMIUM
+                        </a></strong> </td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
@@ -46,19 +80,32 @@
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
                 </tr>
                 <tr>
-                   <td> <strong><a href="#">COMISIONES</a></strong> </td>
+                   <td> <strong>
+                           <a class="nuvv aS colurAd" title="
+                              Sobre las compras de las personas que ingreses a la red y las que ellos ingresen hasta por 6 niveles.">
+                              COMISIONES
+                            </a>
+                        </strong></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
                 </tr>
                 <tr>
-                   <td> <strong><a href="#">BONIFICACIONES</a></strong> </td>
+                   <td> <strong>
+                          <a class="nuvv" title="Por amigos que se inscriban a la red con tu código de usuario y por los que inscriban tus amigos y los amigos de tus amigos hasta 6 niveles de profundidad.">
+                              BONIFICACIONES
+                          </a>
+                        </strong> </td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
                 </tr>
                  <tr>
-                   <td> <strong><a href="#">PRIMAS ANUALES</a></strong> </td>
+                   <td> <strong>
+                          <a class="nuvv" title="Año tras año, los miembros de tu Red renovarán el Derecho de Permanencia y por ello recibirás una prima.">
+                              PRIMAS ANUALES
+                          </a>
+                        </strong> </td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>negativo-rojo.png "></td>
                    <td class="text-center"><img src="<?= BASE_IMG_TIENDA ;?>positivo-verde.png "></td>
--- public/js/tron_terceros_registro.js
+++ public/js/tron_terceros_registro.js
@@ -1,3 +1,25 @@
+
+// Tooltips => Paso 2
+  function simple_tooltip(target_items, name){
+    $(target_items).each(function(i){
+      $("body").append("<div class='"+name+"' id='"+name+i+"'><p>"+$(this).attr('title')+"</p></div>");
+      var my_tooltip = $("#"+name+i);
+
+      $(this).removeAttr("title").mouseover(function(){
+          my_tooltip.css({opacity:0.9, display:"none"}).fadeIn(400);
+      }).mousemove(function(kmouse){
+          my_tooltip.css({left:kmouse.pageX-160, top:kmouse.pageY+20});
+      }).mouseout(function(){
+          my_tooltip.fadeOut(400);
+      });
+    });
+  }
+  $(document).ready(function(){
+     simple_tooltip("a.nuvv","tooltip");
+  });
+
+
+
 //  Formulario de Registro PASO 1.
 
     var $numero_nit      = $('#identificacion');
