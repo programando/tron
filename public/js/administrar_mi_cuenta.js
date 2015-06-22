21af63b9dbb4493e6306c3a1dc4a54fd8f011f7f
programando <jhonjamesmg@hotmail.com>
2015-06-22 09:26:11 -0500

    James

--- public/js/administrar_mi_cuenta.js
+++ public/js/administrar_mi_cuenta.js
@@ -30,57 +30,26 @@ $('#favoritos').on('click',function(){
     $('#cabezera_favoritos').css('display','block');
 });
 
-
-$('#perfil-datos-personales').on('click',function(){
-    $.ajax({
-         dataType: 'html',
-         url:      '/tron/terceros/modificacion_datos/',
-         type:     'post',
-				    success:  function (respuesta)
-				      {
-				         $('.contenedor_cuenta').html('');
-				         $('.contenedor_cuenta').html(respuesta);
-				      }
-				  });
-});
-
-
-$('#perfil-datos-personales').on('click',function(){
-    $.ajax({
-         dataType: 'html',
-         url:      '/tron/terceros/modificacion_datos/',
-         type:     'post',
-				    success:  function (respuesta)
-				      {
-				         $('.contenedor_cuenta').html('');
-				         $('.contenedor_cuenta').html(respuesta);
-				      }
-				  });
-});
-
-
-<<<<<<< HEAD
-$('#informes-pedidos-realizados').on('click',function(){
-	alert('first');
-
-$(".usu-1:first" ).css('background','#003E90');
-$(".usu-1:first" ).css('color','white');
-=======
-});
-
-// Menu sueprior
 $('li .perfil_menu_link').on('click',function(){
 				$('.perfil_menu_link').css('background','#B7B7B7');
 				$(this).css('background','#003E90');
 });
 
-// Seleccion de direccion a atualizar
-$('.contenedor_cuenta').on('click','.direcciones_a_atualizar',function(){
-	   $('.direcciones_a_atualizar').css('background','#B7B7B7');
-	   $(this).css('background','#003E90');
-	   $(this).css('color','white');
->>>>>>> origin/master
+$('#perfil-datos-personales').on('click',function(){
+    $.ajax({
+         dataType: 'html',
+         url:      '/tron/terceros/modificacion_datos/',
+         type:     'post',
+				    success:  function (respuesta)
+				      {
+				         $('.contenedor_cuenta').html('');
+				         $('.contenedor_cuenta').html(respuesta);
+				      }
+				  });
+});
 
+// Seleccion de direccion a atualizar
+$('#informes-pedidos-realizados').on('click',function(){
     $.ajax({
          dataType: 'html',
          url:      '/tron/pedidos/historial_mis_pedidos/',
