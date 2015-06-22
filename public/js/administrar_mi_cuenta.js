bb0c25a38d6bfa1fd4fea267041b01c0bdd22b1b
erickdav <erickdavidmg@gmail.com>
2015-06-22 09:30:08 -0500

    Erick

--- public/js/administrar_mi_cuenta.js
+++ public/js/administrar_mi_cuenta.js
@@ -59,14 +59,6 @@ $('#perfil-datos-personales').on('click',function(){
 });
 
 
-<<<<<<< HEAD
-$('#informes-pedidos-realizados').on('click',function(){
-	alert('first');
-
-$(".usu-1:first" ).css('background','#003E90');
-$(".usu-1:first" ).css('color','white');
-=======
-});
 
 // Menu sueprior
 $('li .perfil_menu_link').on('click',function(){
@@ -79,7 +71,7 @@ $('.contenedor_cuenta').on('click','.direcciones_a_atualizar',function(){
 	   $('.direcciones_a_atualizar').css('background','#B7B7B7');
 	   $(this).css('background','#003E90');
 	   $(this).css('color','white');
->>>>>>> origin/master
+
 
     $.ajax({
          dataType: 'html',
