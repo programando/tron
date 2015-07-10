465478f75ee7f2572371d2ba3d5d9f5cf9bd3a67
programando <jhonjamesmg@hotmail.com>
2015-07-10 14:59:03 -0500

    james

--- application_sections/carrito_botones.php
+++ application_sections/carrito_botones.php
@@ -5,14 +5,14 @@
         <div class="col-lg-12 col-md-12 col-sm-12"><!--Botones -->
           <div class="cont-boton"><!--Contenedor de los botones -->
             <button type="button" class="btn-seguir-comprando">Seguir Comprando</button>
-            <button type="button" class="btn-finalizar-pedido">Finalizar Pedido</button>
+            <button type="button" class="btn-finalizar-pedido">Continuar</button>
           </div><!--Contenedor de los botones -->
         </div><!--Botones -->
 <?php else :  ;?>
 <div class="col-lg-12 col-md-12 col-sm-12"><!--Botones -->
           <div class="cont-boton"><!--Contenedor de los botones -->
             <button type="button" class="btn-seguir-comprando">Seguir Comprando</button>
-            <button type="button" class="btn-forma-pago-pedido">Finalizar Pedido </button>
+            <button type="button" class="btn-forma-pago-pedido">Continuar </button>
           </div><!--Contenedor de los botones -->
         </div><!--Botones -->
 <?php endif ; ?>
