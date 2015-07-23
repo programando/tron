2f511c4c19c11af0e9006ef8f9b03204fc69a4ce
erickdav <erickdavidmg@gmail.com>
2015-07-23 11:34:12 -0500

    erick

--- public/js/resumen_pedido_tron.js
+++ public/js/resumen_pedido_tron.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
 
        if($(this).scrollTop() > 0){
-				        $('.carrito-resumen-pedido').css('margin-top','150px');
+    	 $('.carrito-resumen-pedido').css('margin-top','0px');
 
-				      } else {
-				      	 $('.carrito-resumen-pedido').css('margin-top','0px');
-				      }
+    	      } else {
+    	      	 $('.carrito-resumen-pedido').css('margin-top','0px');
+    	      }
 
     	  if($(this).scrollTop() > 1500){
             $('.carrito-resumen-pedido').addClass('carrito-resumen-pedido-active');
