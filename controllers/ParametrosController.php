551327fb5da1f4eac7541d0bdbc0c88866ad31e7
programando <jhonjamesmg@hotmail.com>
2015-09-02 08:19:45 -0500

    James

--- controllers/ParametrosController.php
+++ controllers/ParametrosController.php
@@ -21,10 +21,12 @@ class ParametrosController extends Controller
 
 						public function Transportadoras_Vr_Kilo_Destino($idmcipio){
 							 echo $idmcipio;
-								 if ( !isset( $idmcipio )){
+							 echo "valor";
+								 if ( !isset( $idmcipio ) || empty( $idmcipio)){
 								 	$idmcipio = 153;
 								 }
-
+							 echo $idmcipio;
+							 echo "valor 2";
 									return $this->Parametros->Transportadoras_Vr_Kilo_Destino($idmcipio);
 						}
 	  }
