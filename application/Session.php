46219f8de702eb8041a32f15ff87953b5e3c4183
programando <jhonjamesmg@hotmail.com>
2015-10-07 11:42:27 -0500

    Jhon James

--- application/Session.php
+++ application/Session.php
@@ -30,9 +30,11 @@
                     }
                 }
               } else {
+                $_SESSION = array();
                 session_destroy();
                 $parametros_cookies = session_get_cookie_params();
                 setcookie(session_name(),0,1,$parametros_cookies["path"]);
+                        Debug::Mostrar( $_SESSION  );
             }
         }
 
