d9142ff499f52e470804fb8fa75792265bd48fd1
ION329 <advanced.ion@gmail.com>
2016-01-09 13:36:56 -0500

    Will - Otros productos - Ene 9 - 13:35
    JJ - Favor ayudarme actualizando la carga del Ajax. Gracias

--- application_sections/otros_productos_categorias.php
+++ application_sections/otros_productos_categorias.php
@@ -1,25 +1,25 @@
-<div class="lateralion">
+<div class="lateralion mb20">
     <div class="panel"><!--aqui va el panel-default = el color del panel -->
         <div class="panel-heading">
-            <div class="panel-title">
-               <a class="colorBlue t24 mb30" data-toggle="collapse" data-parent="#accordion" href="#one">CATEGORÍAS</a>
+            <div class="panel-title colorBlue">
+               <a class="t18" data-toggle="collapse" data-parent="#accordion" href="#one">Categorías</a>
             </div>
         </div>
         <div id="one" class="panel-collapse collapse in"> <!-- collapse in -->
             <div class="panel-body">
-                <ul class="nav" >
-                  <?php foreach ($this->Productos_Categorias_Nv_1  as $Categorias_Nv_1)
-                    {
-                      $orden_nivel_1 = $Categorias_Nv_1['orden_nivel_1'];
-                      $idorden_nv_1  = $Categorias_Nv_1['idorden_nv_1'];   // Id del menu
-                      $nombre_clase  = 'orden_nivel_'.$idorden_nv_1 ;
-                    ?>
-                     <li>
-                        <a  class="lista-productos" id="btn-1" data-toggle="collapse"
+                <ul class="nav">
+					<?php
+                    	foreach ($this->Productos_Categorias_Nv_1  as $Categorias_Nv_1) {
+							$orden_nivel_1 = $Categorias_Nv_1['orden_nivel_1'];
+							$idorden_nv_1  = $Categorias_Nv_1['idorden_nv_1'];   // Id del menu
+							$nombre_clase  = 'orden_nivel_'.$idorden_nv_1 ;
+					?>
+					<li>
+						<a  class="lista-productos cP collapsed" id="btn-1" data-toggle="collapse"
                             data-target="#<?=  $nombre_clase ; ?>"
                             id-categoria ="<?= $idorden_nv_1 ; ?>"
                             nombre-categoria=" <?= $orden_nivel_1 ;?> "
-                            aria-expanded="false"><?= $orden_nivel_1  ;?>
+                            aria-expanded="false"><?= ucwords(strtolower($orden_nivel_1)); ?>
                         </a>
                       </li>
                       <ul class="nav collapse " id="<?= $nombre_clase; ?>" role="menu" aria-labelledby="btn-1">
@@ -28,16 +28,19 @@
                             {
                               $idorden_nv_1_en_nv_2 = $Categorias_Nv_2['idorden_menu_nivel_1']; // Campo que relaciona Categoria y subcategoria
                               $orden_nivel_2        =  $Categorias_Nv_2['orden_nivel_2'] ;
-                              $orden_nivel_2        = $orden_nivel_2  .' (' . $Categorias_Nv_2['cantidad'] . ')';
+                              //$orden_nivel_2        = $orden_nivel_2  .' (' . $Categorias_Nv_2['cantidad'] . ')';
     
                               $idorden_nv_2         = $Categorias_Nv_2['idorden_nv_2'] ;
                               if ( $idorden_nv_1 == $idorden_nv_1_en_nv_2) {
                           ?>
                             <li>
-                                <a  class="sub-lista-productos"
-                                    nombre-subcategoria ="<?=$orden_nivel_2 ?> "
-                                    id-subcategoria="<?=  $idorden_nv_2 ;?>"
-                                  > &nbsp;&nbsp;&nbsp; <?=$orden_nivel_2 ?>
+                                <a class="sub-lista-productos"
+                                    nombre-subcategoria="<?=$orden_nivel_2 ;?> "
+                                    id-subcategoria="<?= $idorden_nv_2 ;?>"
+                                  >
+								  		<span class="cantti t10 rr10"><?= $Categorias_Nv_2['cantidad']; ?></span>
+										<?=$orden_nivel_2 ;?>                                        
+                                        <br style="clear:both;" />
                                 </a>
                             </li>
                           <?php }};?> <!-- $this->Productos_Categorias_Nv_2  -->
--- application_sections/otros_productos_marcas.php
+++ application_sections/otros_productos_marcas.php
@@ -1,28 +1,32 @@
-<div class="panel">
-  <div class="panel-heading  cabezera-panel">
-    <h4 class="panel-title">
-      <a class="marcas" data-toggle="collapse" data-parent="#accordion" href="#two">MARCAS</a>
-    </h4>
-  </div>
-  <div id="two" class="panel-collapse collapse">
-    <div class="panel-body">
-      <nav>
-        <ul class="nav">
-              <?php
-              foreach ($this->Productos_Marcas  as $Marcas)
-              {
-                $idmarca   = $Marcas['idmarca'];   // id de la marca
-                $nom_marca = String_Functions::Formato_Texto($Marcas['nom_marca']);   // Nombre de la marca
-                $cantidad  = $Marcas['cantidad'];   // Nombre de la marca
-                ?>
-                <li>
-                  <a class="lista-marcas" nom-marca="<?=$nom_marca ;?>" id-marca="<?=$idmarca  ;?>">
-                      <?= $nom_marca . ' ( ' . $cantidad . ' )' ?>
-                  </a>
-                </li>
-                <?php };?>
-        </ul>
-      </nav>
-    </div>
-  </div>
+<div class="lateralion">
+    <div class="panel"><!--aqui va el panel-default = el color del panel -->
+        <div class="panel-heading">
+            <div class="panel-title colorBlue">
+               <a class="t16" data-toggle="collapse" data-parent="#accordion" href="#two">Marcas</a>
+            </div>
+        </div>
+
+        <div id="two" class="panel-collapse collapse">
+            <div class="panel-body">
+                <nav>
+                    <ul class="nav">
+                        <?php
+                        foreach ($this->Productos_Marcas  as $Marcas) {
+                            $idmarca   = $Marcas['idmarca'];   // id de la marca
+                            $nom_marca = String_Functions::Formato_Texto($Marcas['nom_marca']);   // Nombre de la marca
+                            $cantidad  = $Marcas['cantidad'];   // Nombre de la marca
+                        ?>
+                            <li>
+                                <a class="lista-marcas cP t14" nom-marca="<?=$nom_marca ;?>" id-marca="<?=$idmarca  ;?>">
+                                    <span class="cantti t10 rr10"><?= $cantidad; ?></span>
+									<?= ucwords(strtolower($nom_marca)); ?>
+                                    <br style="clear:both;" />
+                                </a>
+                            </li>
+                        <?php };?>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+	</div>
 </div>
