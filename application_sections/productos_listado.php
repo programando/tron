90c1f17ec619fc5500fab9b2cdc6d474324a4da8
programando <jhonjamesmg@hotmail.com>
2016-01-12 11:49:33 -0500

    James

--- application_sections/productos_listado.php
+++ application_sections/productos_listado.php
@@ -1,52 +1,52 @@
 <div id="contenedor-productos">
 	<!--IMG = Varias Referencias , Ofertas , Tecnoligias-->
     <div class="imgRefTec">
-    
+
 		<?php if (isset($mostrar_imagen_varias_referencias) and $mostrar_imagen_varias_referencias!=false) : ?>
         	<?php if ($id_agrupacion!=0): ?>
                 <!--IMG-Varias Referencias-->
                 <a href="<?=BASE_URL ;?>productos/varias_referencias/<?=$id_agrupacion ;?> ">
                     <div class="img-varRef" title="Ver Varias Referencias"><!--IMG-varias-referencias -->
-                    
+
                     </div>
                 </a><!--IMG-Varias Referencias-->
         	<?php endif ?>
         <?php endif ?>
-        
-        
+
+
         <?php if ($en_oferta == 1): ?>
             <!--IMG-ofertas -->
             <div class="img-ofertas" title="Producto en Oferta"> </div>
         <?php endif ?>
-        
+
         <?php if ($fabricado_x_ta ==1): ?>
             <!--IMG-tecnologias -->
             <img src="<?=BASE_IMG_PRODUCTOS;?>22.png" width="67" title="Producto Fabricado por Balquimia S.A.S." >
             <!--<div class="img-tecno" title="Producto Fabricado por Balquimia S.A.S."></div>-->
         <?php endif ?>
-    
+
     </div>
 	<!--IMG = Varias Referencias , Ofertas , Tecnoligias-->
-        
-        
-        
-    <div class="taC mb10">    
+
+
+
+    <div class="taC mb10">
         <a href="<?=BASE_URL ;?>productos/vista_ampliada/<?= $idproducto;?>/<?= $Id_Area_Consulta;?> ">
-        
-            <img src="<?=BASE_IMG_PRODUCTOS_232x232. $nombre_imagen;?>" class="mb10" />
-            
+
+            <img src="<?=BASE_IMG_PRODUCTOS_472x472. $nombre_imagen;?>" class="mb10" />
+
             <div class="nomproducto taC t14" id ="<?= $idnomproducto ;?>"><?=$nom_producto ;?></div>
-            
+
             <div class="nompresentacion taC" id ="<?= $idnompresentacion ;?>"><small><?=$nompresentacion ;?></small></div>
-            
-        </a>    
+
+        </a>
     </div>
-    
+
     <div class="taC mb10"><!-- Precio Clientes Ocasional -->
         <strong><?=$pv_ocasional;?></strong>
         <span><img src="<?=BASE_IMG_TIENDA;?>18.png" title="Precio Público / Comprador Ocasional" ></span>
     </div>
-    
+
     <!--PRECIO AMIGO TRON-->
     <div class="priceTRONProd taC mb20" >
         <span  id="<?= $id_precio_final_tron ;?>">  <?=$pv_tron;?> </span>
