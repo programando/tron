f731e7b3d2652355715653a5b276ba9656b93137
programando <jhonjamesmg@hotmail.com>
2016-02-04 10:39:06 -0500

    Jhon James

--- application/Session.php
+++ application/Session.php
@@ -10,10 +10,9 @@
 		public static function Init( ){
             //ini_set("session.use_only_cookies","1");
             //ini_set("session.use_trans_sid","0");
-            $Estoy_Logueado = Session::Get('autenticado');
-            if ( !isset($Estoy_Logueado) || $Estoy_Logueado = FALSE ){
+
 			 session_start();
-            }
+
             //session_set_cookie_params(0, "/");
 		}
 
