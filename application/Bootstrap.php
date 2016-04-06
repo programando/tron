4b45577d6283e7f80a159bc1673a23a0778cffe7
programando <jhonjamesmg@hotmail.com>
2016-04-06 17:31:54 -0500

    James

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -38,8 +38,8 @@ class Bootstrap
             }
 
         } else {
-            Debug::Mostrar( $_SESSION );
-            //throw new Exception(header('Location: ' . BASE_URL .'views/error/404.php'));
+
+            throw new Exception(header('Location: ' . BASE_URL .'views/error/404.php'));
 
 
         }
