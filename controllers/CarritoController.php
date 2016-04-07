92bb294ecd8abe07541fdf1054b30ef8f1a751dd
programando <jhonjamesmg@hotmail.com>
2016-04-07 08:16:44 -0500

    James

--- controllers/CarritoController.php
+++ controllers/CarritoController.php
@@ -355,7 +355,7 @@ class CarritoController extends Controller{
       */
       //VERIFICA SI DENTRO E CARRO EXITEN COMBOS O KIT DE INICIO LOS CUALES NO PUEDES SER COMPRADOS POR EMPRESARIOS O CLIENTES TRON
 
-
+Debug::Mostrar( Session::Get('Cantidad_Novedades') );
       $this->Borrar_Productos_Carro_Plan_2_3();
 
       //----------------------------------------------------------------------------------------------------------------------------
