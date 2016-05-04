71044b135427862d654fdc2176d3b8061ee115b5
programando <jhonjamesmg@hotmail.com>
2016-05-04 15:34:20 -0500

    James

--- application_sections/ventana_modal_explica_columnas_pedido.php
+++ application_sections/ventana_modal_explica_columnas_pedido.php
@@ -12,13 +12,20 @@
        <div class="modal-body">
        	   <div class="container-fluid">
        	   	   <div class="row">
-                  <div class="col-lg-12 col-md-12 col-sm-12">
-                        MENSAJE DE LA VENTANA
+                  <div class=" text-justify col-lg-12 col-md-12 col-sm-12">
+                        Te has registrado como comprador ocasional y al momento de pagar te han salido 2 columnas con valores diferentes.
+Estas 2 columnas son para mostrarte los valores especiales (columna azul) que obtienen nuestros consumidores de productos TRON
+Los consumidores de productos TRON línea Hogar o Industrial que hayan realizado compras de estas líneas por valor de $30.000 en este mes podrán obtener todos los productos de la tienda a precios especiales.
+¿Te animas a echarles un vistazo? Sólo debes consumir $30.000 en alguna de estas 2 líneas y obtendrás los mejores precios.
+
                   	   </p>
                   </div>
        	   	   </div>
        	   </div>
        </div>
+                      <div class="modal-footer">
+                     <button type="button" class="btn btn-info" data-dismiss="modal" aria-hidden="true">Cerrar</button>
+                </div>
    	  </div>
    </div>
 </div>
