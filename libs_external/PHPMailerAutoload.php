1762480243be046be7783eb2e82505852d2e5998
programando <jhonjamesmg@hotmail.com>
2016-05-27 11:15:36 -0500

    JJ

--- libs_external/PHPMailerAutoload.php
+++ libs_external/PHPMailerAutoload.php
@@ -15,6 +15,7 @@
  * @note This program is distributed in the hope that it will be useful - WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.
+ * define autoload
  */
 
 /**
