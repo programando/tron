bd65fef8689b6f5e1500da235ce165319b8d4b62
programando <jhonjamesmg@hotmail.com>
2016-05-27 16:21:17 -0500

    JJ

--- controllers/EmailsController.php
+++ controllers/EmailsController.php
@@ -236,7 +236,48 @@
 
 
 
+public function correo(){
+  //Incluimos la clase de PHPMailer
+//require_once('../libs_external/class.phpmailer.php')
 
+  $miemail  = CORREO_CONTACTOS; // 'jj94mg@gmail.com';
+  $pass    = PASS_CORREO_CONTACTOS  ; //'lacasallena';
+
+     //CORREO_CONTACTOS . ' ' . PASS_CORREO_CONTACTOS;
+
+  $correo = new PHPMailer();
+
+   $correo->IsSMTP();
+   $correo->SMTPDebug     = 2;
+   $correo->SMTPAuth      = true;
+   $correo->IsHTML        = true;                      // enable SMTP authentication
+   $correo->ContentType   = "text/html";
+   $correo->CharSet       = "utf-8";
+   $correo->SMTPSecure    = 'ssl';                     // sets the prefix to the servier
+   $correo->Host          = 'smtp.gmail.com';         // sets GMAIL as the SMTP server
+   $correo->Port          = 465;
+   $correo->SMTPKeepAlive = true;
+   $correo->Mailer        = "smtp";                   // set the SMTP port
+   $correo->Username      = $miemail;         // GMAIL username
+   $correo->Password      = $pass;    // GMAIL password
+   $correo->From          = $miemail;
+   $correo->FromName      = 'TRON Entre amigos alcanzamos';
+   $correo->Subject       = 'Prueba de Correo' ;
+   $correo->AltBody       = ""; //Text Body
+   $correo->WordWrap      = 50; // set word wrap                                // send as HTML
+   $correo->AddAddress('jhonjamesmg@hotmail.com');
+   $correo->Body      ='mensaje de prueba' . rand(1,150);
+
+  if(!$correo->Send()) {
+      echo rand(1,150);
+    echo "Hubo un error: " . $correo->ErrorInfo;
+  } else {
+     echo rand(1,150);
+    echo "Mensaje enviado con exito." ;
+  }
+
+
+}
 
     public function Enviar_Correo(){
         if ( $this->Email->Send()){
