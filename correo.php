6b080527134ff4464ea3c58bed69684edc270718
programando <jhonjamesmg@hotmail.com>
2016-05-27 10:09:53 -0500

    JJ

--- correo.php
+++ correo.php
@@ -8,14 +8,14 @@ $pass = 'lacasallena';
 $correo = new PHPMailer();
 
  $correo->IsSMTP();
- $correo->SMTPDebug     = 2;
+ $correo->SMTPDebug     = 0;
  $correo->SMTPAuth      = true;
  $correo->IsHTML        = true;              				 // enable SMTP authentication
  $correo->ContentType   = "text/html";
  $correo->CharSet       = "utf-8";
- $correo->SMTPSecure    = 'ssl';                     // sets the prefix to the servier
+ $correo->SMTPSecure    = 'tls';                     // sets the prefix to the servier
  $correo->Host          = 'smtp.gmail.com';      		// sets GMAIL as the SMTP server
- $correo->Port          = 465;
+ $correo->Port          = 587;
  $correo->SMTPKeepAlive = true;
  $correo->Mailer        = "smtp";                   // set the SMTP port
  $correo->Username      = $email;					// GMAIL username
@@ -31,7 +31,7 @@ $correo = new PHPMailer();
 if(!$correo->Send()) {
   echo "Hubo un error: " . $correo->ErrorInfo;
 } else {
-  echo "Mensaje enviado con exito.";
+  echo "Mensaje enviado con exito." . rand(1,150);;
 }
 
 ?>
