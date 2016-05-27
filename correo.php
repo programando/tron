74098305aef0a4701c17ffafd0aa552a2122a72d
programando <jhonjamesmg@hotmail.com>
2016-05-27 10:16:18 -0500

    JJ

--- correo.php
+++ correo.php
@@ -13,9 +13,9 @@ $correo = new PHPMailer();
  $correo->IsHTML        = true;              				 // enable SMTP authentication
  $correo->ContentType   = "text/html";
  $correo->CharSet       = "utf-8";
- $correo->SMTPSecure    = 'tls';                     // sets the prefix to the servier
+ $correo->SMTPSecure    = 'ssl';                     // sets the prefix to the servier
  $correo->Host          = 'smtp.gmail.com';      		// sets GMAIL as the SMTP server
- $correo->Port          = 587;
+ $correo->Port          = 465;
  $correo->SMTPKeepAlive = true;
  $correo->Mailer        = "smtp";                   // set the SMTP port
  $correo->Username      = $email;					// GMAIL username
@@ -31,7 +31,7 @@ $correo = new PHPMailer();
 if(!$correo->Send()) {
   echo "Hubo un error: " . $correo->ErrorInfo;
 } else {
-  echo "Mensaje enviado con exito." . rand(1,150);;
+  echo "Mensaje enviado con exito." . rand(1,150);
 }
 
 ?>
