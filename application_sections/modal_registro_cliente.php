9488a60b8de1c5903ac543fed05c16da69b81cee
ION329 <advanced.ion@gmail.com>
2017-04-27 10:52:06 -0500

    Modales de registro de clientes y empresarios

---
+++ application_sections/modal_registro_cliente.php
@@ -0,0 +1,20 @@
+<!-- Ventana modal =  IMG de recomendar negocio a amigo -->
+ <div class="modal fade" id="modal_registro_cliente" tabindex="-1" role="dialog" aria-labelledby="modal_registro_cliente" aria-hidden="true">
+  <div class="modal-dialog" style="width:832px; max-width:100%;">
+    <div class="modal-content">
+      <!-- Cuepro -->
+      <div class="modal-body">
+          <div>
+              <a href="../"><img src="<?= BASE_IMG_EMPRESA ?>registro_cliente.jpg"></a>
+          </div>
+      </div>
+    </div><!-- modal-content -->
+  </div><!-- modal-dialog -->
+</div>
+
+
+
+
+
+
+
