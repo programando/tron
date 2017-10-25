8388a2ade91587891c993a77afb695019c8ebc37
programando <jhonjamesmg@hotmail.com>
2017-10-25 16:53:35 -0500

    JJ
    Retiro linea textil

---
+++ application_sections/productos/tron/carros_y_motos_old.php
@@ -0,0 +1,38 @@
+
+<div class="ionix">
+    <div class="general">
+		<div class="contenido-index mb30 pAA30">
+
+
+            <!-- INICIA PRESENTACIÓN DE PRODUCTOS -->
+            <div id="contenido-productos">
+				<?php
+                    $mostrar_imagen_varias_referencias = true;
+
+                    Session::Set('mostrar_un_solo_precio', TRUE );
+                    foreach ($this->Productos_Carros_Motos as $Productos){
+
+
+                        include (APPLICATION_CODS . 'campos_productos.php');
+                ?>
+                     <?php if ( $en_oferta == 1 ) : ?> <div class="artOpt artPor4 oferta">
+                        <?php else :?>                 <div class="artOpt artPor4">
+                     <?php endif ;?>
+
+                     <?php include (APPLICATION_SECTIONS . 'productos_listado.php');?>
+
+                      </div>
+
+                <?php } ?>
+                <br style="clear:both;" /><br /><br />
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+</div>
+
