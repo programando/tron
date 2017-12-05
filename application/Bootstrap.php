e4975c46388222de046343067fad19d65a9d1189
programando <jhonjamesmg@hotmail.com>
2017-12-04 20:48:39 -0500

    JJ

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -49,7 +49,7 @@ class Bootstrap
             echo '<br> Metodo <br>';
             echo $Metodo ;
 
-/*
+
             // Desde aqui se carga el contraolador con o sin argumentos.... carpeta controllers
             if(isset($args)){
                 call_user_func_array(array( $Controller, $Metodo ), $args);
@@ -58,7 +58,7 @@ class Bootstrap
                 call_user_func(array($Controller, $Metodo));
             }
 
-            */
+
 
         } else {
 
