c016ec546d0fe2e61ef642edbb817ec1d8b11e44
programando <jhonjamesmg@hotmail.com>
2017-12-06 18:33:30 -0500

    JJ

--- application_sections/redtron_btn_superior2.php
+++ application_sections/redtron_btn_superior2.php
@@ -2,7 +2,7 @@
 <div>
     <div class="tabion">
         <a class="tab-off taC" href="<?= BASE_URL ;?>redtron/red_de_amigos_tron">RED DE AMIGOS TRON</a>
-        <a class="tab-on taC" href="<?= BASE_URL ;?>redtron/planes_de_registro">TIENDA TRON</a>
+        <a class="tab-on taC" href="<?= BASE_URL ;?>redtron/planes_registro">TIENDA TRON</a>
         <br style="clear:both;" />
     </div>
 </div>
--- application_sections/redtron_tienda_active.php
+++ application_sections/redtron_tienda_active.php
@@ -2,7 +2,7 @@
     <div class="col-lg-12  col-md-12  col-sm-12">
      <div class="tabs">
       <a class="tab-red"    href="<?= BASE_URL ;?>redtron/red_de_amigos_tron"><div class="campos-pagina-red" id="tab-inative-red">RED DE AMIGOS TRON</div></a>
-      <a class="tab-tienda" href="<?= BASE_URL ;?>redtron/planes_de_registro"><div class="campos-pagina-tienda" id="tabs-tienda-active">  TIENDA TRON  </div></a>
+      <a class="tab-tienda" href="<?= BASE_URL ;?>redtron/planes_registro"><div class="campos-pagina-tienda" id="tabs-tienda-active">  TIENDA TRON  </div></a>
     </div>
     </div>
 <!--Campos De La Pagina RED DE AMIGOS TRON-->
