cb7003962ab137f72555eeb3d7b3c01645f67dc6
programando <jhonjamesmg@hotmail.com>
2019-11-28 12:40:19 -0500

    Deb

--- controllers/FacturaElectronicaController.php
+++ controllers/FacturaElectronicaController.php
@@ -407,8 +407,11 @@ class FacturaElectronicaController extends Controller
 
         private function xmlInicioArchivo( $NombreArchivo, $TipoDocumento  ) {
 
+          header("Content-Type: text/html/force-download");
+          header("Content-Disposition: attachment; filename='Factura.xml'");
+
           $this->xml = new XMLWriter();
-         $this->xml->openURI ( $NombreArchivo.".xml" );
+         $this->xml->openURI ( 'php://output');
          $this->xml->setIndent(true);
          $this->xml->setIndentString("\t");
 
