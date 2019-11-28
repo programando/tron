acb3300fdd1b9631bc8a75194b01eb4f705b28e0
programando <jhonjamesmg@hotmail.com>
2019-11-28 13:07:32 -0500

    Deb

--- controllers/FacturaElectronicaController.php
+++ controllers/FacturaElectronicaController.php
@@ -408,7 +408,7 @@ class FacturaElectronicaController extends Controller
         private function xmlInicioArchivo( $NombreArchivo, $TipoDocumento  ) {
 
          $this->xml = new XMLWriter();
-         $this->xml->openURI ('/opt/lampp/htdocs/tron/public/files/Factura.xml');
+         $this->xml->openURI ('file:///opt/lampp/htdocs/tron/public/files/Factura.xml');
          $this->xml->setIndent(true);
          $this->xml->setIndentString("\t");
 
