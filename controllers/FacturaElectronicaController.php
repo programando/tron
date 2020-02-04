a6af354b05d3a8201eaf71e83f4fbf39af0a8fc8
programando <jhonjamesmg@hotmail.com>
2020-02-03 19:28:33 -0500

    Desc

--- controllers/FacturaElectronicaController.php
+++ controllers/FacturaElectronicaController.php
@@ -315,7 +315,7 @@ class FacturaElectronicaController extends Controller
 
       private function DSC () {
         if ( empty($this->DSC )  )                    return ;
-        if (  $this->DSC['_03_vr_dscto'] === 0   ) ) return ;
+        if (  $this->DSC['_03_vr_dscto'] === 0   )  return ;
           $this->xml->startElement('DSC');
             $this->CrearSiExite('DSC_1',   $this->DSC['_01_tp_dscto']           );
             $this->CrearSiExite('DSC_2',   $this->DSC['_02_pctje']              );
